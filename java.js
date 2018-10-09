c6593c5e0517a048e85c9ed3c0bd0b61ea1d9e7f
saba161 <saba.koguashvili1999@mail.ru>
2018-10-09 01:10:47 -0700

    object

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-var mycar = {
+/* var mycar = {
 	"color" : "red",
 	"year" : "2006",
 	"model" : "cls 63 amg"
@@ -8,3 +8,19 @@ var car = mycar["year"];
 alert(car);
 var car1 = mycar["model"];
 alert(car1);
+*/
+var football = {
+	"players" : {
+		"goal" : {
+			"cxriani" : "dvalishvili",
+			"sto piati" : "ronaldo"
+		},
+		"number" : {
+			"number 10" : "messi",
+			"number 7" : "ronaldo"
+		}
+	}
+};
+
+var foot = football.players.goal["cxriani"];
+alert(foot);
