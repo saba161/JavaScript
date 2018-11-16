cf7f4306a3f32c2eee84aa1f472e63a38d99ccf9
saba161 <saba.koguashvili1999@mail.ru>
2018-11-16 23:29:24 +0400

    javascript

--- java.js
+++ java.js
@@ -2,3 +2,9 @@ function insert(act){
 	var numsarea = document.getElementById("numsarea");
 	numsarea.value = numsarea.value + act;
 }
+
+function hide() 
+{
+	var elem = document.getElementById("calc");
+	elem.style.display = 'none';
+}
