30433ab05dacb8d8cc47f8774e58485f83ae6e93
saba161 <saba.koguashvili1999@mail.ru>
2018-11-17 14:36:21 +0400

    script

--- java.js
+++ java.js
@@ -1,17 +1,21 @@
+//calculator operations
 function insert(act)
 {
 	var numsarea = document.getElementById("numsarea");
 	numsarea.value = numsarea.value + act;
 }
 
-function hide() 
+function backspace()
+{    
+    var rekensom  = document.getElementById('numsarea').value;
+    document.getElementById('numsarea').value = rekensom.substring(0,rekensom.length -1);
+}
+//show history
+function show() 
 {
 	var elem = document.getElementById("calc");
 	elem.style.display = 'none';
 }
 
-var backspace = function()
-{    
-    var rekensom  = document.getElementById('numsarea').value;
-    document.getElementById('numsarea').value=rekensom.substring(0,rekensom.length -1);
-}
+
+
